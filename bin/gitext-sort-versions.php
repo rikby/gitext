ebcf8f195c84a17564e3dd5a7cfb911ec0ba7d46
Andrew Roslik <andkirby@gmail.com>
2016-08-18 18:41:16 +0300

    Added using bin file.

---
+++ bin/gitext-sort-versions.php
@@ -0,0 +1,2 @@
+<?php
+require_once __DIR__.'/../src/shell/version-sort.php';
--- src/Console/Command/Install.php
+++ src/Console/Command/Install.php
@@ -61,7 +61,7 @@ class Install extends AbstractCommand
         return [
             'git tags'       => sprintf(
                 trim(file_get_contents(realpath(__DIR__.'/../../shell/command/git-tags'))),
-                str_replace('\\', '/', realpath(__DIR__.'/../../shell/version-sort.php'))
+                str_replace('\\', '/', realpath(__DIR__.'/../../../bin/gitext-sort-versions.php'))
             ),
             'git tag-move'   => trim(file_get_contents(realpath(__DIR__.'/../../shell/command/git-tag-move'))),
             'git tag-remove' => trim(file_get_contents(realpath(__DIR__.'/../../shell/command/git-tag-remove'))),
