b7656b8349e1fb2f3e7f0b830833c896b0f1bdeb
Andrew Roslik <andkirby@gmail.com>
2017-06-14 11:26:10 +0300

    Added getting GitFlow namespace on checkout onto a remote branch or a tag.

--- src/shell/git-flow-namespace-branch.sh
+++ src/shell/git-flow-namespace-branch.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
 branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ -z "${branch}" ] || [ "${branch}" == 'HEAD' ]; then
+  # disable parsing asterisks, or set -f
+  set -o noglob
+
+  # get current branch
+  branch=$(git branch | grep '*' | grep -Eo '[A-z]+/[^)]+')
+
+  # enable parsing asterisks
+  set +o noglob
+
+  # Trim remote name
+  branch=$(echo "${branch}" | sed -r 's:^'$(git remote | xargs echo | tr ' ' '|')'::' | sed -r 's:^/::')
+fi
+
 switch_branch=''
 if [[ ${branch} =~ \/ ]]; then
-    IFS='/' read -ra branch <<< "${branch}"
-    for i in "${branch[@]}"; do
-        switch_branch=${i}
-        break
-    done
+  IFS='/' read -ra branch <<< "${branch}"
+  for i in "${branch[@]}"; do
+    switch_branch=${i}
+    break
+  done
 fi
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
