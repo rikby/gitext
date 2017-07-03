651d7ccfe3c263d099d215f53555dffcd601a84f
Andrew Roslik <andkirby@gmail.com>
2017-07-03 17:23:16 +0300

    Implemented #1: Add `git flow-feature-root` command  - Added using feature prefix config. - Added removing section.

--- src/shell/git-flow-feature-root.sh
+++ src/shell/git-flow-feature-root.sh
@@ -22,23 +22,25 @@ has_flow_config
 if [ -n "${namespace:-}" ]; then
   # Validate namespace
   # It should be already created
-  has_branch_error "feature/${namespace}" \
-    "No such branch 'feature/${namespace}' to define GitFlow Root Feature."
+  has_branch_error "$(git config gitflow.prefix.feature)${namespace}" \
+    "No such branch '$(git config gitflow.prefix.feature)${namespace}' to define GitFlow Root Feature."
 
   # Store config for recovery
   git config gitext.gitflow.branch.develop-default    $(git config gitflow.branch.develop)
   git config gitext.gitflow.prefix.feature-default    $(git config gitflow.prefix.feature)
   # Set git flow configuration
-  git config gitflow.branch.develop    feature/${namespace}
-  git config gitflow.prefix.feature    feature/${namespace}-
+  git config gitflow.branch.develop    $(git config gitflow.prefix.feature)${namespace}
+  git config gitflow.prefix.feature    $(git config gitflow.prefix.feature)${namespace}-
   echo
   echo -e "  GitFlow Root Feature = ${t_color_yellow}${namespace}${t_default}"
 elif [ -n "$(git config gitext.gitflow.branch.develop-default)" ]; then
   # Set git flow configuration
   git config gitflow.branch.develop    $(git config gitext.gitflow.branch.develop-default)
   git config gitflow.prefix.feature    $(git config gitext.gitflow.prefix.feature-default)
+  git config --remove-section gitext.gitflow.branch
+  git config --remove-section gitext.gitflow.prefix
   echo
   echo -e "  GitFlow Root Feature has been ${t_color_red}removed${t_default}."
 else
-  check_error 2 'GitFlow Root Feature was not initiated. Nothing to rollback.'
+  check_error 2 'GitFlow Root Feature is not set.'
 fi
