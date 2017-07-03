c24c4e025343cce6c2d012fb638281e9fa64d50c
Andrew Roslik <andkirby@gmail.com>
2017-07-03 15:36:14 +0300

    Implemented #1: Add `git flow-feature-root` command  - Added setting gitflow old config values. - Added checking init status.

--- src/shell/git-flow-feature-root.sh
+++ src/shell/git-flow-feature-root.sh
@@ -13,26 +13,35 @@ __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 readonly __dir __file
 
 . ${__dir}/lib/common.sh
+. ${__dir}/lib/git.sh
 
 namespace="${1:-}"
+
+if [ -z "$(git config gitflow.branch.develop)" ]; then
+  check_error 2 'It looks like GitFlow is not initiated. Please use command '\
+"\n    git flow init"
+fi
 
 if [ -n "${namespace:-}" ]; then
   # Validate namespace
   # It should be already created
-  if ! git branch | tr '*' ' ' \
-      | sed 's/^ *//;s/ *$//' | grep "feature/${namespace}"; then
-    check_error 2 "No such branch 'feature/${namespace}' to define GitFlow Root Feature."
-  fi
+  has_branch_error "feature/${namespace}" \
+    "No such branch 'feature/${namespace}' to define GitFlow Root Feature."
 
+  # Store config for recovery
+  git config gitext.gitflow.branch.develop-default    $(git config gitflow.branch.develop)
+  git config gitext.gitflow.branch.feature-default    $(git config gitflow.branch.feature)
   # Set git flow configuration
   git config gitflow.branch.develop    feature/${namespace}
   git config gitflow.prefix.feature    feature/${namespace}-
   echo
   echo -e "  GitFlow Root Feature = ${t_color_yellow}${namespace}${t_default}"
-else
+elif [ -n "$(git config gitext.gitflow.branch.develop-default)" ]; then
   # Set git flow configuration
-  git config gitflow.branch.develop    develop
-  git config gitflow.prefix.feature    feature/
+  git config gitflow.branch.develop    $(git config gitext.gitflow.branch.develop-default)
+  git config gitflow.branch.feature    $(git config gitext.gitflow.branch.feature-default)
   echo
   echo -e "  GitFlow Root Feature has been ${t_color_red}removed${t_default}."
+else
+  check_error 2 'GitFlow Root Feature was not initiated. Nothing to rollback.'
 fi
