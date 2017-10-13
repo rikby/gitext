7fc284a17da80fa0de2f99391b8f8e5a95095df7
Andrew Roslik <andkirby@gmail.com>
2017-10-13 14:37:12 +0300

    @@through Fixed sorting issue. There was a but with cutting some versions from a list.

--- src/shell/lib/version-sort.php
+++ src/shell/lib/version-sort.php
@@ -4,7 +4,7 @@
 /**
  * Sort versions list
  */
-require_once __DIR__ . '/../../../bin/autoload-init.php';
+require_once __DIR__.'/../../../bin/autoload-init.php';
 //use Naneau\SemVer\Sort;
 
 try {
@@ -12,7 +12,7 @@ try {
         throw new Exception('This file can be run in CLI only.');
     }
     //remove file from the args
-    $serverArgs = (array)$_SERVER['argv'];
+    $serverArgs = (array) $_SERVER['argv'];
     array_shift($serverArgs);
 
     if (!$serverArgs) {
@@ -20,10 +20,10 @@ try {
         exit(1);
     }
 
-    $versionsRaw = implode(' ', $serverArgs);
-
-    $versions = preg_replace('#\+[^\n]+#', '', $versionsRaw); //Remove meta information after sign "+"
-    $versions = preg_split("(\n|\s)", $versions);
+    $versions = preg_split(
+        "(\n|\s)",
+        implode(' ', $serverArgs)
+    );
 
     $originalVersions = [];
     foreach ($versions as $key => $original) {
@@ -35,17 +35,27 @@ try {
     }
 
     /**
-     * Cannot use naneau/semver to to a problem with patches
+     * Cannot use naneau/semver to to a problem with patches.
+     * But it looks like SemVer doesn't describe patches at all on http://semver.org.
+     *
      * @link https://github.com/naneau/semver/issues/24
      */
 //    $versions = call_user_func_array(Sort::class . '::sort', $versions);
 
     array_walk($originalVersions, 'trim');
-    usort($originalVersions, 'version_compare');
+    usort(
+        $originalVersions,
+        function ($a, $b) {
+            list($a) = explode('+', $a);
+            list($b) = explode('+', $b);
+
+            return version_compare($a, $b);
+        }
+    );
     $versions = $originalVersions;
 
     echo implode(PHP_EOL, $versions);
 } catch (Exception $e) {
-    echo 'error: ' . $e->getMessage();
+    echo 'error: '.$e->getMessage();
     exit(1);
 }
